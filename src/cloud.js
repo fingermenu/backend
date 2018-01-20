1c18a8170e433ff28e49bc7342863e9cc42acb44
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-01-21 10:48:59 +1300

    Packages upgraded

--- src/cloud.js
+++ src/cloud.js
@@ -1,13 +1,40 @@
 // @flow
 
-import { languageLoaderByKey, languageLoaderById, tableStateLoaderById, tagLoaderById } from '@fingermenu/backend-graphql';
+import {
+  choiceItemLoaderById,
+  languageLoaderByKey,
+  languageLoaderById,
+  menuLoaderById,
+  menuItemLoaderById,
+  sizeLoaderById,
+  tableStateLoaderByKey,
+  tableStateLoaderById,
+  tagLoaderById,
+} from '@fingermenu/backend-graphql';
+
+Parse.Cloud.afterSave('ChoiceItem', async () => {
+  choiceItemLoaderById.clearAll();
+});
 
 Parse.Cloud.afterSave('Language', async () => {
   languageLoaderById.clearAll();
   languageLoaderByKey.clearAll();
 });
 
+Parse.Cloud.afterSave('Menu', async () => {
+  menuLoaderById.clearAll();
+});
+
+Parse.Cloud.afterSave('MenuItem', async () => {
+  menuItemLoaderById.clearAll();
+});
+
+Parse.Cloud.afterSave('Size', async () => {
+  sizeLoaderById.clearAll();
+});
+
 Parse.Cloud.afterSave('TableState', async () => {
+  tableStateLoaderByKey.clearAll();
   tableStateLoaderById.clearAll();
 });
 
