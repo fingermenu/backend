114a8e1bacfde44dbb9b94c95fbea8ffc1ca949c
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-02-17 09:47:59 +1300

    Bug fixed

--- src/cloud.js
+++ src/cloud.js
@@ -16,70 +16,70 @@ import {
   tagLoaderById,
 } from '@fingermenu/backend-graphql';
 
-Parse.Cloud.afterSave('ChoiceItem', (request) => {
+Parse.Cloud.afterSave('ChoiceItem', request => {
   if (request.object.createdAt !== request.object.updatedAt) {
     choiceItemLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('ChoiceItemPrice', (request) => {
+Parse.Cloud.afterSave('ChoiceItemPrice', request => {
   if (request.object.createdAt !== request.object.updatedAt) {
     choiceItemPriceLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('Language', (request) => {
+Parse.Cloud.afterSave('Language', request => {
   if (request.object.createdAt !== request.object.updatedAt) {
     languageLoaderById.clear(request.object.id);
     languageLoaderByKey.clear(request.object.get('key'));
   }
 });
 
-Parse.Cloud.afterSave('Menu', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    menuLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('Menu', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    menuLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('MenuItem', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    menuItemLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('MenuItem', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    menuItemLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('MenuItemPrice', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    menuItemPriceLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('MenuItemPrice', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    menuItemPriceLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('Restaurant', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    restaurantLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('Restaurant', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    restaurantLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('Size', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    sizeLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('Size', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    sizeLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('Table', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    tableLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('Table', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    tableLoaderById.clear(request.object.id);
   }
 });
 
-Parse.Cloud.afterSave('TableState', (request) => {
+Parse.Cloud.afterSave('TableState', request => {
   if (request.object.createdAt !== request.object.updatedAt) {
     tableStateLoaderByKey.clear(request.object.id);
     tableStateLoaderById.clear(request.object.get('key'));
   }
 });
 
-Parse.Cloud.afterSave('Tag', (request) => {
-  if (request.objeect.createdAt !== request.objeect.updatedAt) {
-    tagLoaderById.clear(request.objeect.id);
+Parse.Cloud.afterSave('Tag', request => {
+  if (request.object.createdAt !== request.object.updatedAt) {
+    tagLoaderById.clear(request.object.id);
   }
 });
